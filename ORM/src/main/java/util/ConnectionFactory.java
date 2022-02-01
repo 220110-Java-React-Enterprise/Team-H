16a1313f8f1ebb97c6aef18791a828f1ae6ea964
Mahmood Sardari <mahmoudsardari@gmail.com>
2022-02-01 16:15:11 -0600

    ConnectionManager

--- ORM/src/main/java/util/ConnectionFactory.java
+++ ORM/src/main/java/util/ConnectionFactory.java
@@ -1,10 +1,25 @@
 package util;
 
-public class ConnectionFactory
-{
+import java.sql.Connection;
+import java.sql.SQLException;
 
-	private ConnectionFactory(String host, int port, String dbname, String username, String password, String schema, String driver)
-	{
+public class ConnectionFactory {
 
-	}
+    private static ConnectionFactory connectionFactory;
+    private static Connection connection;
+
+    private ConnectionFactory(String host, int port, String dbname, String username, String password, String schema, String driver) {
+
+    }
+
+    public static Connection getConnection(String host, int port, String dbname, String username, String password, String schema, String driver) {
+        if (connectionFactory == null) {
+            connectionFactory = new ConnectionFactory(host, port, dbname, username, password, schema, driver);
+        }
+        return connection;
+    }
+    public static void closeConnection() throws SQLException {
+        connection.close();
+        connectionFactory = null;
+    }
 }
