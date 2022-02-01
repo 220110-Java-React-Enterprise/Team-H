e361149d4c6b447441f5cbfee0542952c1fb8cd9
Alvin <huerox@msn.com>
2022-02-01 14:32:24 -0800

    completed connection factory.

--- ORM/src/main/java/util/ConnectionFactory.java
+++ ORM/src/main/java/util/ConnectionFactory.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
@@ -8,18 +9,26 @@ public class ConnectionFactory {
     private static ConnectionFactory connectionFactory;
     private static Connection connection;
 
-    private ConnectionFactory(String host, int port, String dbname, String username, String password, String schema, String driver) {
+    private ConnectionFactory(String host, int port, String dbname, String username, String password) throws SQLException {
 
+        String connectionStr = "jdbc:mariadb://" +
+                host + ":" + port + "/" +
+                dbname + "?user=" + username +
+                "&password=" + password;
+
+        connection = DriverManager.getConnection(connectionStr);
     }
 
-    public static Connection getConnection(String host, int port, String dbname, String username, String password, String schema, String driver) {
+    public static Connection getConnection(String host, int port, String dbname, String username, String password) throws SQLException {
         if (connectionFactory == null) {
-            connectionFactory = new ConnectionFactory(host, port, dbname, username, password, schema, driver);
+            connectionFactory = new ConnectionFactory(host, port, dbname, username, password);
         }
         return connection;
     }
+    
     public static void closeConnection() throws SQLException {
         connection.close();
         connectionFactory = null;
     }
+
 }
