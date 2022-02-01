37fce32fb85f8f2eb0bbccc05231a8a30cf6ebe8
jlor993 <jlor993@gmail.com>
2022-02-01 15:25:06 -0600

    ORM constructor

---
+++ ORM/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package util;
+
+public class ConnectionFactory
+{
+
+	private ConnectionFactory(String host, int port, String dbname, String username, String password, String schema, String driver)
+	{
+
+	}
+}
