aa81bad882c9b41ecca8740f8f7f56b64ca31cfb
jlor993 <jlor993@gmail.com>
2022-02-02 10:35:31 -0600

    ORM pseudocode

--- ORM/src/main/java/scriptors/Scriptor.java
+++ ORM/src/main/java/scriptors/Scriptor.java
@@ -4,22 +4,66 @@ package scriptors;
  * Object Relational Mapping class used to convert data between incompatible type systems using an OOP based language.
  */
 
-public class Scriptor
-{
-
-	String sql = "SELECT * FROM table WHERE id = ?";
-
-	String firstPart = "SELECT * FROM ";
-	String tableName = "";        //reflect on the object to get this
-	String nextPart = " WHERE ";
-	String filterColumn = "";     //reflect upon the object for this data as well
-	String parameterList = " = ?";
-
-	// Append full SQL Script
-	String sqlScript = firstPart + tableName + nextPart + filterColumn + parameterList;
-
-}
-
 /**
  * Mention to team how you now have a better understanding of the scriptor for your ORM!
  */
+
+public class Scriptor
+{
+
+	// Not sure what to make the parameter, it's just Object for now
+	public static String createSQL(Object o)
+	{
+		//INSERT INTO table (_) VALUES (?)
+
+		String part1 = "INSERT INTO ";
+		String tableName = ""; // reflection or annotation
+		String part2 = " (";
+		String columnList = ""; // iterate over fields and use reflection or annotations
+		String part3 = ") VALUES (";
+		String values = "?, ?"; // num of ? should be equal to number of fields
+		String part4 = ")";
+		return part1 + tableName + part2 + columnList + part3 + values + part4;
+	}
+
+	public static String readSQL(Object o)
+	{
+		// SELECT * FROM table WHERE _ = ?
+
+		String part1 = "SELECT * FROM ";
+		String tableName = "";
+		String part2 = " WHERE ";
+		String columnList = "";
+		String parameterList = " = ?";
+
+		return part1 + tableName + part2 + columnList + parameterList;
+	}
+
+	public static String updateSQL(Object o)
+	{
+		// UPDATE table SET _ = ? WHERE _ = ?
+
+		String part1 = "UPDATE ";
+		String tableName = "";
+		String part2 = " SET ";
+		String updateValues = "column = value"; // iterate over and separate by commas
+		String part3 = " WHERE ";
+		String condition = "primaryKey = value";
+
+		return part1 + tableName + part2 + updateValues + part3 + condition;
+	}
+
+	public static String deleteSQL(Object o)
+	{
+		// DELETE FROM table WHERE _ = ?
+
+		String part1 = "DELETE FROM ";
+		String tableName = "";
+		String part2 = " WHERE ";
+		String columnList = "";
+		String parameterList = " = ?";
+
+		return part1 + tableName + part2 + columnList + parameterList;
+	}
+
+}
